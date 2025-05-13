f4e9bba2fcd3cd97d01a42bc5ea38d8e28170c3a
Sampath R P R <127502999+RavinduSampath@users.noreply.github.com>
2025-05-13 15:54:41 +0530

    navigation

--- lib/screens/Hair.dart
+++ lib/screens/Hair.dart
@@ -6,6 +6,7 @@ class HairCarePage extends StatefulWidget {
   const HairCarePage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _HairCarePageState createState() => _HairCarePageState();
 }
 
